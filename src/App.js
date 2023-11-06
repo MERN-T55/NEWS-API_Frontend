141ad4e6f68cfd26c2d54db9d1f575cf23dc867f
Praveenna2802 <143382660+Praveenna2802@users.noreply.github.com>
2023-11-06 11:18:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Nav from "./Components/Nav.js";
+import LoginPage from "./Components/LoginPage.js"; 
+import RegisterPage from "./Components/RegisterPage.js";
 
 function App() {
   return (
     <HashRouter>
       <div>
-        <Nav />
-        
+        <Routes>
+          <Route path="/" element={<Nav />} /> 
+          <Route path="/login" element={<LoginPage />} /> 
+          <Route path="/register" element={<RegisterPage />}/>
+        </Routes>
       </div>
     </HashRouter>
   );
