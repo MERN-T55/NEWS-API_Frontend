36fd3464f91c251c0903a1383fb147d7636b54e8
Praveenna2802 <143382660+Praveenna2802@users.noreply.github.com>
2023-11-08 23:28:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import News from "./news";
 import Nav from "./nav";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
 import {BrowserRouter,Switch,Route,Link} from "react-router-dom"
 import reportWebVitals from "./reportWebVitals";
 
@@ -10,11 +12,14 @@ ReactDOM.render(
   <BrowserRouter>
   <>
   <Nav />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<RegisterPage />} />
     <Route exact path="/Apple"><News newsName="iphone" /></Route>
     <Route path="/Tesla"><News newsName="tesla" /></Route>
     <Route path="/Bitcoin"><News newsName="bitcoin" /></Route>
     <Route path="/nasa"><News newsName="nasa" /></Route>
     <Route path="/upsc"><News newsName="upsc" /></Route>
+    
   </>
   </BrowserRouter>,
   document.getElementById("root")
